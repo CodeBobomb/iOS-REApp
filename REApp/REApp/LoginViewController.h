8c69e5e1035d5b83ce5ae2a885fe7dc2a6b81115
vatoN1 <demirovict@gmail.com>
2014-12-08 19:46:15 +0100

    login click event

---
+++ REApp/REApp/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  REApp
+//
+//  Created by academy387 on 12/8/14.
+//  Copyright (c) 2014 Academy387. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *loginBtn;
+
+
+
+@end
